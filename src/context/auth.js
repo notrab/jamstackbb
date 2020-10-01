507761bd43270cfa9e9460039047cd595cc5b9d7
Jamie Barton <jamie@notrab.dev>
2020-10-01 23:53:24 +0100

    stubbed auth dispatch methods

--- src/context/auth.js
+++ src/context/auth.js
@@ -3,12 +3,25 @@ import { createContext, useReducer, useContext } from "react";
 const AuthDispatchContext = createContext();
 const AuthStateContext = createContext();
 
+const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+const LOGOUT = "LOGOUT";
+
 const initialState = {
   isAuthenticated: false,
+  user: null,
+  token: null,
 };
 
 function reducer(state, { payload, type }) {
   switch (type) {
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        ...payload,
+        isAuthenticated: true,
+      };
+    case LOGOUT:
+      return initialState;
     default:
       throw new Error(`Unhandled action type ${type}`);
   }
@@ -17,8 +30,24 @@ function reducer(state, { payload, type }) {
 function AuthProvider({ children }) {
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  const login = async ({ email, password }) => {
+    const token = "abc123";
+    const user = { name: "Jamie" };
+
+    dispatch({ type: LOGIN_SUCCESS, payload: { token, user } });
+  };
+
+  const createUser = async ({ name, email, password }) => {
+    const token = "abc123";
+    const user = { name: "Jamie" };
+
+    dispatch({ type: LOGIN_SUCCESS, payload: { token, user } });
+  };
+
+  const logout = () => dispatch({ type: LOGOUT });
+
   return (
-    <AuthDispatchContext.Provider>
+    <AuthDispatchContext.Provider value={{ login, createUser, logout }}>
       <AuthStateContext.Provider value={state}>
         {children}
       </AuthStateContext.Provider>
