6bcbb48ad0137a69de6e6cb1deded8c03a770b2f
Jamie Barton <jamie@notrab.dev>
2020-10-03 15:17:38 +0100

    feat: login/register forms (#17)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
+import Link from "next/link";
 import { useAuthState, useAuthDispatch } from "../context/auth";
 
 export default function IndexPage() {
   const { isAuthenticated, user } = useAuthState();
-  const { login, register, logout } = useAuthDispatch();
+  const { logout } = useAuthDispatch();
 
   return (
     <>
@@ -13,27 +14,8 @@ export default function IndexPage() {
         </>
       ) : (
         <>
-          <button
-            onClick={() =>
-              login({
-                email: "j@jjj.com",
-                password: "abc123",
-              })
-            }
-          >
-            Login
-          </button>
-          <button
-            onClick={() =>
-              register({
-                name: "Nathan",
-                email: "j@jjj.com",
-                password: "abc123",
-              })
-            }
-          >
-            Register
-          </button>
+          <Link href="/login">Login</Link>
+          <Link href="/register">Register</Link>
         </>
       )}
     </>
