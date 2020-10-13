e93e64384251c97b1beecc2e44c6511882791517
Jamie Barton <jamie@notrab.dev>
2020-10-13 14:08:01 +0100

    chore: remove useMemo

--- src/pages/members.js
+++ src/pages/members.js
@@ -1,4 +1,3 @@
-import { useMemo } from "react";
 import useSWR from "swr";
 import formatRelative from "date-fns/formatRelative";
 import differenceInMinutes from "date-fns/differenceInMinutes";
@@ -51,17 +50,17 @@ export default function MembersPage({ initialData }) {
 
       <div className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3">
         {data.users.map(({ created_at, id, last_seen, name }) => {
-          const formattedJoinedAt = useMemo(
-            () =>
-              formatRelative(Date.parse(created_at), today, {
-                weekStartsOn: 1,
-              }),
-            [created_at]
+          const formattedJoinedAt = formatRelative(
+            Date.parse(created_at),
+            today,
+            {
+              weekStartsOn: 1,
+            }
           );
 
-          const formattedLastSeen = useMemo(
-            () => differenceInMinutes(today, Date.parse(last_seen)),
-            [last_seen]
+          const formattedLastSeen = differenceInMinutes(
+            today,
+            Date.parse(last_seen)
           );
 
           const isUserOnline = 20 >= formattedLastSeen;
@@ -79,9 +78,9 @@ export default function MembersPage({ initialData }) {
                       <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
                     </svg>
                   </span>
-                  {isUserOnline ? (
+                  {isUserOnline && (
                     <span className="bg-green-500 w-2 h-2 absolute top-0 right-0 rounded-full" />
-                  ) : null}
+                  )}
                 </span>
                 <div>
                   <h3 className="md:text-lg font-semibold text-gray-800">
